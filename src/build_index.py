05597ad0af53539882b68c219dfa1a2efe4dac7a
ecordovaa <efcordovaa@gmail.com>
2021-12-02 20:58:02 -0500

    improve index file

--- src/build_index.py
+++ src/build_index.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     # Collecting dimension data.
     for raw_csv in file_dir:
         print(f"Processing: {raw_csv}")
-        dimensions = ["Pickup_longitude", "Pickup_latitude"]
+        dimensions = ["Pickup_longitude", "Pickup_latitude", "Dropoff_longitude", "Dropoff_latitude"]
         raw_df = pd.read_csv(f"data/{raw_csv}", skipinitialspace=True, usecols=dimensions)
         raw_df.to_csv(idx, mode="a", index=False)
         print(f"**Merged {len(raw_df)} rows.")
@@ -22,6 +22,6 @@ if __name__ == "__main__":
     df = pd.read_csv(idx)
     print("Cleaning odd values.")
     df.dropna()
-    df = df[(df["Pickup_longitude"] != '0.0') & (df["Pickup_latitude"] != '0.0')]
+    df = df[(df["Pickup_longitude"] != '0.0') & (df["Pickup_latitude"] != '0.0') & (df["Dropoff_latitude"] != '0.0') & (df["Dropoff_latitude"] != '0.0')]
     df.to_csv(idx, index=False)
     print(f"**Built an index with {len(df)} rows in {timeit.default_timer() - start}s.")
